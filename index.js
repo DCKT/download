5a2e185086a84005f8077805d5c043c00ec561f7
Thomas Deconinck <dck@outlook.fr>
2018-04-02 00:57:03 +0200

    expose getFilename

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-'use strict';
-const fs = require('fs');
-const path = require('path');
-const url = require('url');
-const caw = require('caw');
-const contentDisposition = require('content-disposition');
-const decompress = require('decompress');
-const filenamify = require('filenamify');
-const getStream = require('get-stream');
-const got = require('got');
-const makeDir = require('make-dir');
-const pify = require('pify');
-const pEvent = require('p-event');
-const fileType = require('file-type');
-const extName = require('ext-name');
+"use strict";
+const fs = require("fs");
+const path = require("path");
+const url = require("url");
+const caw = require("caw");
+const contentDisposition = require("content-disposition");
+const decompress = require("decompress");
+const filenamify = require("filenamify");
+const getStream = require("get-stream");
+const got = require("got");
+const makeDir = require("make-dir");
+const pify = require("pify");
+const pEvent = require("p-event");
+const fileType = require("file-type");
+const extName = require("ext-name");
 
 const fsP = pify(fs);
 const filenameFromPath = res => path.basename(url.parse(res.requestUrl).pathname);
 
 const getExtFromMime = res => {
-	const header = res.headers['content-type'];
+	const header = res.headers["content-type"];
 
 	if (!header) {
 		return null;
@@ -33,8 +33,8 @@ const getExtFromMime = res => {
 	return exts[0].ext;
 };
 
-const getFilename = (res, data) => {
-	const header = res.headers['content-disposition'];
+module.exports.getFilename = (res, data) => {
+	const header = res.headers["content-disposition"];
 
 	if (header) {
 		const parsed = contentDisposition.parse(header);
@@ -58,7 +58,7 @@ const getFilename = (res, data) => {
 };
 
 module.exports = (uri, output, opts) => {
-	if (typeof output === 'object') {
+	if (typeof output === "object") {
 		opts = output;
 		output = null;
 	}
@@ -69,37 +69,42 @@ module.exports = (uri, output, opts) => {
 		protocol = protocol.slice(0, -1);
 	}
 
-	opts = Object.assign({
-		encoding: null,
-		rejectUnauthorized: process.env.npm_config_strict_ssl !== 'false'
-	}, opts);
+	opts = Object.assign(
+		{
+			encoding: null,
+			rejectUnauthorized: process.env.npm_config_strict_ssl !== "false"
+		},
+		opts
+	);
 
-	const agent = caw(opts.proxy, {protocol});
-	const stream = got.stream(uri, Object.assign({agent}, opts));
+	const agent = caw(opts.proxy, { protocol });
+	const stream = got.stream(uri, Object.assign({ agent }, opts));
 
-	const promise = pEvent(stream, 'response').then(res => {
-		const encoding = opts.encoding === null ? 'buffer' : opts.encoding;
-		return Promise.all([getStream(stream, {encoding}), res]);
-	}).then(result => {
-		// TODO: Use destructuring when targeting Node.js 6
-		const data = result[0];
-		const res = result[1];
+	const promise = pEvent(stream, "response")
+		.then(res => {
+			const encoding = opts.encoding === null ? "buffer" : opts.encoding;
+			return Promise.all([getStream(stream, { encoding }), res]);
+		})
+		.then(result => {
+			// TODO: Use destructuring when targeting Node.js 6
+			const data = result[0];
+			const res = result[1];
 
-		if (!output) {
-			return opts.extract ? decompress(data, opts) : data;
-		}
+			if (!output) {
+				return opts.extract ? decompress(data, opts) : data;
+			}
 
-		const filename = opts.filename || filenamify(getFilename(res, data));
-		const outputFilepath = path.join(output, filename);
+			const filename = opts.filename || filenamify(getFilename(res, data));
+			const outputFilepath = path.join(output, filename);
 
-		if (opts.extract) {
-			return decompress(data, path.dirname(outputFilepath), opts);
-		}
+			if (opts.extract) {
+				return decompress(data, path.dirname(outputFilepath), opts);
+			}
 
-		return makeDir(path.dirname(outputFilepath))
-			.then(() => fsP.writeFile(outputFilepath, data))
-			.then(() => data);
-	});
+			return makeDir(path.dirname(outputFilepath))
+				.then(() => fsP.writeFile(outputFilepath, data))
+				.then(() => Promise.resolve(data, filename, outputFilepath));
+		});
 
 	stream.then = promise.then.bind(promise);
 	stream.catch = promise.catch.bind(promise);
